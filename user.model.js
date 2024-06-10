c8c311be5ed1149ae4f0b73f51e4f02fdf9df2cd
Harsh Tiwari <145565378+HarshTiwari7080@users.noreply.github.com>
2024-06-10 22:04:49 +0530

    Add files via upload

---
+++ user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    firstname: { type: String, required: true },
+    email: { type: String, required: true },
+    lastname: { type: String, required: true },
+    password: { type: String, required: true }
+}, { timestamps: true });
+
+const usermodel = mongoose.model('user', userSchema);
+
+module.exports = usermodel;
